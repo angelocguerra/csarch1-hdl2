4c029cde1cdbcb501b652d6130e3d8ede418640e
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-07-17 15:10:28 +0800

    Added initial comments

---
+++ YGueJ.v
@@ -0,0 +1,6 @@
+// GUERRA, Jose Maria Angelo C. (S17)
+
+`timescale 1ns / 1ps
+
+// Module Name: Structural Modeling
+// Boolean Function: F = A (CD + B) + BC'
---
+++ YGueJ_tb.v
@@ -0,0 +1,7 @@
+// GUERRA, Jose Maria Angelo C. (S17)
+
+`timescale 1ns / 1ps
+
+// Module Name: Structural Modeling
+// Boolean Function: F = A (CD + B) + BC'
+
