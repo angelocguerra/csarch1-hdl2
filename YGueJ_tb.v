a63afe94af2c808b99ffdf4c4bbbaabef8ec35c7
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-07-21 05:07:03 +0800

    Fixed loop sequence

--- YGueJ_tb.v
+++ YGueJ_tb.v
@@ -33,7 +33,7 @@ module som_dec_2x4_pope_behav_tb();
             // Enable = 1 (Positive)
             t_input = 4'b0000;
             t_E = 1;
-            for(i = 1; i <= 17; i++)
+            for(i = 1; i < 17; i++)
                 #10 t_input = i; // Every 10ns, value 0000 becomes 0001 and so on until it goes back to 0000
         end
 
@@ -44,7 +44,7 @@ module som_dec_2x4_pope_behav_tb();
         $display("F = A (CD + B) + BC'");
         $display("2x4 Positive Output, Positive Enable Decoder");
 
-        $monitor("Time = %03d | A = %b B = %b C = %b D = %b | E = %b | Output_F = %b",
+        $monitor("Time = %03d | A = %b B = %b C = %b D = %b | en = %b | F = %b",
                  $time, t_input[3], t_input[2], t_input[1], t_input[0], t_E, t_F);
         $dumpfile("YGueJ_dump.vcd");
         $dumpvars();
